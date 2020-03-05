ffa6e5ce788582b375d117077beac1b5e634353b
martacolombas <martacolombas@gmail.com>
2020-03-05 15:00:25 +0100

    feat: Added fragmentMatcher in InMemoryCache

--- client/src/ApiClient.js
+++ client/src/ApiClient.js
@@ -1,14 +1,22 @@
-import { ApolloClient } from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloClient, IntrospectionFragmentMatcher } from 'apollo-boost';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { gql } from 'apollo-boost';
+
+const fragmentMatcher = new IntrospectionFragmentMatcher({
+	introspectionQueryResultData: {
+		__schema: {
+			types: [],
+		},
+	},
+});
 
 const GRAPHQL_ENDPOINT =
 	JSON.parse(localStorage.getItem('enterpriseUrl')) ||
 	'https://api.github.com/graphql';
 
-const cache = new InMemoryCache();
+const cache = new InMemoryCache({ fragmentMatcher });
 const link = new createHttpLink({
 	uri: `${GRAPHQL_ENDPOINT}`,
 });
