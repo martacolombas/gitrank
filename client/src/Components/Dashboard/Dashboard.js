23e5543e74cdefaeded9de5b7cbf7e82028bd4c1
martacolombas <martacolombas@gmail.com>
2020-03-11 13:28:53 +0100

    feat: added authors filter

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -5,7 +5,7 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { useQuery } from '@apollo/react-hooks';
 import './Dashboard.css';
 import PrList from '../PrList/PrList';
-import { GET_PRS, GET_REPOS } from '../../ApiClient/ApiClient';
+import { GET_PRS, GET_REPOS, GET_AUTHORS } from '../../ApiClient/ApiClient';
 import Filter from '../Filter/Filter';
 import { groupPRs, filterByRepos, groupAllRepos } from './utils';
 import TransitionPage from '../TransitionPage/TransitionPage';
@@ -26,9 +26,9 @@ function Dashboard({ className, username }) {
 			? JSON.parse(localStorage.getItem('selectedRepos'))
 			: []
 	);
-	const [selectedOwner, setSelectedOwner] = useState(
-		localStorage.getItem('selectedOwner')
-			? JSON.parse(localStorage.getItem('selectedOwner'))
+	const [selectedAuthor, setSelectedAuthor] = useState(
+		localStorage.getItem('selectedAuthor')
+			? JSON.parse(localStorage.getItem('selectedAuthor'))
 			: []
 	);
 
@@ -50,34 +50,49 @@ function Dashboard({ className, username }) {
 		},
 	});
 
+	const {
+		loading: authorsLoading,
+		data: authorsData,
+		error: authorsError,
+	} = useQuery(GET_AUTHORS, {
+		variables: {
+			login: `${username}`,
+		},
+	});
+
 	// FILTER OPTIONS
 	let options = [];
-	let allOptions = [];
-	let ownerOptions = []; // this variable is necessary to filter options (above) by owner
-	let authors = [];
+	let authorOptions = [];
 
 	if (reposData) {
-		allOptions = groupAllRepos(reposData).map(element => {
+		options = groupAllRepos(reposData).map(element => {
 			return {
 				value: element.id,
 				label: element.nameWithOwner,
-				ownerId: element.owner.id,
-				login: element.owner.login,
-			};
-		});
-
-		ownerOptions = Array.from(
-			new Set(allOptions.map(element => element.ownerId))
-		).map(id => {
-			return {
-				value: id,
-				label: allOptions.find(element => element.ownerId === id).login,
 			};
 		});
 	}
 
+	if (authorsData) {
+		authorOptions = groupPRs(authorsData)
+			.map(element => {
+				return {
+					value: element.author.id,
+					label: element.author.login,
+				};
+			})
+			.reduce((acc, current) => {
+				const x = acc.find(item => item.value === current.value);
+				if (!x) {
+					return acc.concat([current]);
+				} else {
+					return acc;
+				}
+			}, []);
+	}
+
 	// TRANSITION PAGES
-	if (error) {
+	if (error || authorsError || reposError) {
 		console.error(error);
 		return (
 			<TransitionPage
@@ -87,23 +102,13 @@ function Dashboard({ className, username }) {
 		);
 	}
 
-	if (loading) {
+	if (loading || authorsLoading || reposLoading) {
 		return (
 			<TransitionPage
 				src='https://octodex.github.com/images/hula_loop_octodex03.gif'
 				children='Fetching your PRs...'
 			/>
 		);
-	}
-
-	// DATA MANIPULATION -OPTIONS
-	if (selectedOwner && selectedOwner.length) {
-		const selectedIds = selectedOwner.map(element => element.value);
-		options = allOptions.filter(element =>
-			selectedIds.includes(element.ownerId)
-		);
-	} else {
-		options = [...allOptions];
 	}
 
 	// DATA MANIPULATION -PRS
@@ -124,19 +129,21 @@ function Dashboard({ className, username }) {
 				className='Dashboard-sidebar'
 				content={
 					<>
-						<Filter
-							options={ownerOptions}
-							className='Dashboard-filter'
-							value={selectedOwner}
-							placeholder='Select the owner'
-							onChange={value => {
-								setSelectedOwner(value);
-								localStorage.setItem(
-									'selectedOwner',
-									JSON.stringify(value)
-								);
-							}}
-						/>
+						{
+							<Filter
+								options={authorOptions}
+								className='Dashboard-filter'
+								value={selectedAuthor}
+								placeholder='Select the PR author'
+								onChange={value => {
+									setSelectedAuthor(value);
+									localStorage.setItem(
+										'selectedAuthor',
+										JSON.stringify(value)
+									);
+								}}
+							/>
+						}
 						<Filter
 							options={options}
 							className='Dashboard-filter'
