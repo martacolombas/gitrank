60fc2099e60415c3572dfc7c14a5571080e216e6
martacolombas <martacolombas@gmail.com>
2020-03-14 13:21:12 +0100

    feat: added error handling in BE API

--- server/queries.js
+++ server/queries.js
@@ -2,55 +2,109 @@ const pool = require('./db');
 pool.connect();
 
 const getUsers = async (request, response) => {
-	const { rows } = await pool.query('SELECT * FROM users ORDER BY id ASC');
-	response.status(200).send(rows);
+	try {
+		const { rows } = await pool.query('SELECT * FROM users ORDER BY id ASC');
+		response.status(200).send(rows);
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 const getUserById = async (request, response) => {
-	const id = parseInt(request.params.id);
-	const { rows } = await pool.query('SELECT * FROM users WHERE id=$1', [id]);
-	response.status(200).send(rows[0]);
+	try {
+		const id = parseInt(request.params.id);
+		if (id) {
+			const { rows } = await pool.query('SELECT * FROM users WHERE id=$1', [
+				id,
+			]);
+			response.status(200).send(rows[0]);
+		} else {
+			response.status(204);
+			console.log(`Couldn't find ${id}`);
+		}
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 const createUser = async (request, response) => {
-	const { username, password, token } = request.body;
-	const {
-		rows,
-	} = await pool.query(
-		'INSERT INTO users (username, password, token) VALUES ($1, $2, $3) RETURNING *',
-		[username, password, token]
-	);
-	response.status(201).send(rows[0]);
+	try {
+		const { username, password, token } = request.body;
+		const {
+			rows,
+		} = await pool.query(
+			'INSERT INTO users (username, password, token) VALUES ($1, $2, $3) RETURNING *',
+			[username, password, token]
+		);
+		response.status(201).send(rows[0]);
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 const updateUserToken = async (request, response) => {
-	const id = parseInt(request.params.id);
-	const { token } = request.body;
-	const {
-		rows,
-	} = await pool.query('UPDATE users SET token=$1 WHERE id = $2 RETURNING *', [
-		token,
-		id,
-	]);
-	response.status(201).send(rows);
+	try {
+		const id = parseInt(request.params.id);
+		if (id) {
+			const { token } = request.body;
+			const {
+				rows,
+			} = await pool.query(
+				'UPDATE users SET token=$1 WHERE id = $2 RETURNING *',
+				[token, id]
+			);
+			response.status(201).send(rows);
+		} else {
+			response.status(204);
+			console.log(`Couldn't find ${id}`);
+		}
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 const updateUserPassword = async (request, response) => {
-	const id = parseInt(request.params.id);
-	const { password } = request.body;
-	const {
-		rows,
-	} = await pool.query(
-		'UPDATE users SET password=$1 WHERE id = $2 RETURNING *',
-		[password, id]
-	);
-	response.status(201).send(rows);
+	try {
+		const id = parseInt(request.params.id);
+		if (id) {
+			const { password } = request.body;
+			const {
+				rows,
+			} = await pool.query(
+				'UPDATE users SET password=$1 WHERE id = $2 RETURNING *',
+				[password, id]
+			);
+			response.status(201).send(rows);
+		} else {
+			response.status(204);
+			console.log(`Couldn't find ${id}`);
+		}
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 const deleteUser = async (request, response) => {
-	const id = parseInt(request.params.id);
-	const { rows } = await pool.query('DELETE FROM users WHERE id = $1', [id]);
-	response.status(201).send(rows);
+	try {
+		const id = parseInt(request.params.id);
+		if (id) {
+			const { rows } = await pool.query('DELETE FROM users WHERE id = $1', [
+				id,
+			]);
+			response.status(201).send(rows);
+		} else {
+			response.status(204);
+			console.log(`Couldn't find ${id}`);
+		}
+	} catch (error) {
+		response.status(500);
+		console.error(`Error ${error} occurred`);
+	}
 };
 
 module.exports = {
